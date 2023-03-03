a4623771572c2ae90aeb70b06094bc86a3fb1075
Nikos Karampinas <n.karampinas@obi4wan.com>
2023-03-03 15:14:55 +0100

    Resolved issue with sharing jpgs
    Also, an issue where denial would be thrown when tried to preview the image in the share modal

--- src/android/CDVInstagramPlugin.java
+++ src/android/CDVInstagramPlugin.java
@@ -1,7 +1,7 @@
 /*
     The MIT License (MIT)
     Copyright (c) 2013 Vlad Stirbu
-    
+
     Permission is hereby granted, free of charge, to any person obtaining
     a copy of this software and associated documentation files (the
     "Software"), to deal in the Software without restriction, including
@@ -9,10 +9,10 @@
     distribute, sublicense, and/or sell copies of the Software, and to
     permit persons to whom the Software is furnished to do so, subject to
     the following conditions:
-    
+
     The above copyright notice and this permission notice shall be
     included in all copies or substantial portions of the Software.
-    
+
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -24,11 +24,19 @@
 
 package com.vladstirbu.cordova;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Base64;
+import android.util.Log;
+
+import androidx.core.content.FileProvider;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -37,15 +45,11 @@ import org.apache.cordova.PluginResult.Status;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
-import android.util.Base64;
-import android.util.Log;
-import androidx.core.content.FileProvider;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.List;
 
 @TargetApi(Build.VERSION_CODES.FROYO)
 public class CDVInstagramPlugin extends CordovaPlugin {
@@ -58,12 +62,12 @@ public class CDVInstagramPlugin extends CordovaPlugin {
     };
 
     CallbackContext cbContext;
-    
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        
+
         this.cbContext = callbackContext;
-        
+
         if (action.equals("share")) {
             String imageString = args.getString(0);
             String captionString = args.getString(1);
@@ -80,7 +84,7 @@ public class CDVInstagramPlugin extends CordovaPlugin {
         }
         return false;
     }
-    
+
     private void isInstalled() {
         try {
             this.webView.getContext().getPackageManager().getApplicationInfo("com.instagram.android", 0);
@@ -91,13 +95,13 @@ public class CDVInstagramPlugin extends CordovaPlugin {
     }
 
     private void share(String imageString, String captionString) {
-        if (imageString != null && imageString.length() > 0) { 
-            byte[] imageData = Base64.decode(imageString, 0);
-            
-            File file = null;  
-            FileOutputStream os = null;
-            
+        if (imageString != null && imageString.length() > 0) {
+            byte[] imageData = Base64.decode(imageString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
+
+            File file = null;
             File parentDir = this.webView.getContext().getExternalFilesDir(null);
+
             File[] oldImages = parentDir.listFiles(OLD_IMAGE_FILTER);
             for (File oldImage : oldImages) {
                 oldImage.delete();
@@ -105,48 +109,55 @@ public class CDVInstagramPlugin extends CordovaPlugin {
 
             try {
                 file = File.createTempFile("instagram", ".png", parentDir);
-                os = new FileOutputStream(file, true);
+                FileOutputStream out = new FileOutputStream(file, true);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+                out.flush();
+                out.close();
             } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            try {
-                os.write(imageData);
-                os.flush();
-                os.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
 
             Intent shareIntent = new Intent(Intent.ACTION_SEND);
             shareIntent.setType("image/*");
+            Uri uri;
 
             if (Build.VERSION.SDK_INT < 26) {
-                // Handle the file uri with pre Oreo method    
-                shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+                // Handle the file uri with pre Oreo method
+                uri = Uri.fromFile(file);
+                shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
             } else {
                 // Handle the file URI using Android Oreo file provider
                 FileProvider FileProvider = new FileProvider();
 
-                Uri photoURI = FileProvider.getUriForFile(
+                uri = FileProvider.getUriForFile(
                         this.cordova.getActivity().getApplicationContext(),
                         this.cordova.getActivity().getPackageName() + ".provider",
                         file);
 
-                shareIntent.putExtra(Intent.EXTRA_STREAM, photoURI);
+                shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
                 shareIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             }
 
             shareIntent.putExtra(Intent.EXTRA_TEXT, captionString);
             shareIntent.setPackage("com.instagram.android");
 
-            this.cordova.startActivityForResult((CordovaPlugin) this, Intent.createChooser(shareIntent, "Share to"), 12345);   
+            Intent chooser = Intent.createChooser(shareIntent, "Share to");
+
+            List<ResolveInfo> resInfoList = this.cordova.getActivity().getPackageManager()
+                    .queryIntentActivities(chooser, PackageManager.MATCH_DEFAULT_ONLY);
+
+            for (ResolveInfo resolveInfo : resInfoList) {
+                String packageName = resolveInfo.activityInfo.packageName;
+                this.cordova.getActivity().grantUriPermission(packageName, uri,
+                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            }
+
+            this.cordova.startActivityForResult((CordovaPlugin) this, chooser, 12345);
         } else {
             this.cbContext.error("Expected one non-empty string argument.");
         }
     }
-    
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {
